a24714d2b91b7cb41ab7720b2ae7f61082d0cacf
GoodnightJames <128565926+GoodnightJames@users.noreply.github.com>
2023-06-02 02:44:07 +0700

    Update populate_images.php

--- populate_images.php
+++ populate_images.php
@@ -5,29 +5,37 @@ if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
-// Delete existing records from the images table
-$db->query("DELETE FROM images");
+// Define the image data
+$images = [
+    [
+        'image_path' => 'image1.jpg',
+        'votes' => 0
+    ],
+    [
+        'image_path' => 'image2.jpg',
+        'votes' => 0
+    ],
+    [
+        'image_path' => 'image3.jpg',
+        'votes' => 0
+    ],
+    [
+        'image_path' => 'image4.jpg',
+        'votes' => 0
+    ]
+    // Add more image data as needed
+];
 
-$directory = '/Applications/XAMPP/xamppfiles/htdocs/aiCar/images/';
-$images = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+// Prepare and execute the SQL statement to insert the image data
+$stmt = $db->prepare("INSERT INTO images (image_path, votes) VALUES (?, ?)");
+$stmt->bind_param("si", $imagePath, $votes);
 
 foreach ($images as $image) {
-    $sql = "INSERT INTO images (image_path, votes, last_vote) VALUES (?, 0, NOW())";
-
-    if ($stmt = $db->prepare($sql)) {
-        $stmt->bind_param("s", $image);
-
-        if ($stmt->execute()) {
-            echo "Successfully inserted $image\n";
-        } else {
-            echo "Error inserting $image: " . $stmt->error . "\n";
-        }
-
-        $stmt->close();
-    } else {
-        echo "Error preparing statement: " . $db->error . "\n";
-    }
+    $imagePath = $image['image_path'];
+    $votes = $image['votes'];
+    $stmt->execute();
 }
 
+$stmt->close();
 $db->close();
 ?>
